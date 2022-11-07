f6fa12ed2ccbaf59adcd52ebe7322c85f17ca593
bemmanue <76536030+bemmanue@users.noreply.github.com>
2022-11-07 20:50:05 +0300

    Delete unnecessary docker script

--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-#docker rm mariadb
-docker image rm $(docker images -a -q)
-#docker image rm srcs_mariadb
-docker volume prune 
